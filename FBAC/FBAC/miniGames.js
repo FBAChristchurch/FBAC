82b670adfd1b1a9e8623da907612bf8aef75812f
FBAChristchurch <titusgabrielnavacilla@gmail.com>
2024-08-02 18:48:17 +1200

    Add files via upload

---
+++ FBAC/FBAC/miniGames.js
@@ -0,0 +1,55 @@
+let fishType = (Math.random());
+let money = 0
+console.log(fishType);
+
+document.addEventListener("keyup", event =>{
+    let fishType = (Math.random());
+    console.log(fishType);
+    if (fishType <= .3){
+        document.getElementById("text").innerHTML = "caught common fish"
+        money =+ money + 1
+        document.getElementById("money").innerHTML = money
+    }
+    if (fishType >= .3){
+        if (fishType <= .4){
+            document.getElementById("text").innerHTML = "caught uncommon fish"
+            money =+ money + 5
+            document.getElementById("money").innerHTML = money
+        }
+    }
+    if (fishType >= .4){
+        if (fishType <= .475){
+            document.getElementById("text").innerHTML = "caught rare fish"
+            money =+ money + 10
+            document.getElementById("money").innerHTML = money
+        }
+    }
+    if (fishType >= .475){
+        if (fishType <= .525){
+            document.getElementById("text").innerHTML = "caught ultra rare fish"
+            money =+ money + 50
+            document.getElementById("money").innerHTML = money
+        }
+    }
+    if (fishType >= .525){
+        if (fishType <= .55){
+            document.getElementById("text").innerHTML = "caught ledgendary fish"
+            money =+ money + 100
+            document.getElementById("money").innerHTML = money
+        }
+    }
+    if (fishType >= .55){
+        if (fishType <= .8){
+            document.getElementById("text").innerHTML = "caught no fish"
+            document.getElementById("money").innerHTML = money
+        }
+    }
+    if (fishType >= .999){
+        if (fishType <= .9999){
+            document.getElementById("text").innerHTML = "I love My Hero Acedamia - Kyle"
+            document.getElementById("money").innerHTML = money
+        }
+    }
+});
+
+document.getElementById("money").innerHTML = money
